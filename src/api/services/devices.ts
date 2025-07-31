e52bdc7cea55234c51f27d7b427157e7e2d2a737
Tomas Listiak <tomas@listiak.dev>
2025-07-31 18:15:58 +0200

    feat: add random ID generator + move utils

--- src/api/services/devices.ts
+++ src/api/services/devices.ts
@@ -1,6 +1,6 @@
 import { api } from '@/api/client'
 import { Devices } from '@/api/types/device'
-import { getApiUrl } from '@/api/utils'
+import { getApiUrl } from '@/utils'
 
 export const getDevices = async (): Promise<Devices> => {
   const url = getApiUrl({ path: '/fingerprint/ui/public.json' })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,6 @@ import { API_BASE_URL } from '@/config'
 
 export const getApiUrl = ({ path }: { path: string }) =>
   `${API_BASE_URL}${path}`
+
+export const generateId = (prefix = 'id'): string =>
+  `${prefix}-${Math.random().toString(36).substr(2, 9)}`
