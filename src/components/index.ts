765a071f7f1361270b595d7142b8bc1d4cf446f3
Tomas Listiak <tomas@listiak.dev>
2025-08-01 15:31:02 +0200

    chore: cleanup

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,14 +4,5 @@ export * from './common'
 // Layout components
 export * from './layout'
 
-// UI components
-// export * from './ui'
-
 // Icon components
 export * from './icons'
-
-// Feature components
-// export * from './features'
-
-// Form components
-// export * from './forms'
